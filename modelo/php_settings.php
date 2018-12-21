456429f05d75dc006087720fafa450fd7d8e36a5
Julian <Julian@DESKTOP-884R94L>
2018-12-21 09:28:14 -0500

    arreglos varios

--- modelo/php_settings.php
+++ modelo/php_settings.php
@@ -6,9 +6,9 @@
 $host="localhost";
 $user="techno";
 $pw="techno";
-$db="tss";
+$db="tss_centro1";
  
-const HOST="localhost", USER="techno",PW="techno",DB="tss"; //para uso 
+const HOST="localhost", USER="techno",PW="techno",DB="tss_centro1"; //para uso 
 
 /* Para un servidor la combinacion deberá ser $TipoPC="Server"; $TipoKardex="Caja";
  * Para una Caja la combinacion deberá ser $TipoPC="Caja"; $TipoKardex="Caja";
